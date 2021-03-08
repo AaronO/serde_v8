5eb8750ef40a7acfdb3a31b763f140fb46ab1374
Aaron O'Mullan <aaron.omullan@gmail.com>
2021-03-08 06:49:02 +0100

    benches: ser_struct_v8_manual

--- benches/ser.rs
+++ benches/ser.rs
@@ -56,3 +56,25 @@ dualbench!(ser_int_v8, ser_int_json, 12345);
 dualbench!(ser_array_v8, ser_array_json, vec![1,2,3,4,5,6,7,8,9,10]);
 dualbench!(ser_str_v8, ser_str_json, "hello world");
 dualbench!(ser_tuple_v8, ser_tuple_json, (1,false));
+
+#[bench]
+fn ser_struct_v8_manual(b: &mut Bencher) {
+    serdo(|scope| {
+        let v = MathOp{arg1: 10, arg2: 123, operator: None};
+        b.iter(|| {
+           let obj = v8::Object::new(scope);
+           let k1 = v8::String::new(scope, "arg1").unwrap();
+           let k2 = v8::String::new(scope, "arg2").unwrap();
+           let k3 = v8::String::new(scope, "operator").unwrap();
+        //    let k1 = v8::String::new_from_utf8(scope, "arg1".as_ref(), v8::NewStringType::Internalized).unwrap();
+        //    let k2 = v8::String::new_from_utf8(scope, "arg2".as_ref(), v8::NewStringType::Internalized).unwrap();
+        //    let k3 = v8::String::new_from_utf8(scope, "operator".as_ref(), v8::NewStringType::Internalized).unwrap();
+           let v1 = v8::Number::new(scope, v.arg1 as f64);
+           let v2 = v8::Number::new(scope, v.arg2 as f64);
+           let v3 = v8::null(scope);
+           obj.set(scope, k1.into(), v1.into()).unwrap();
+           obj.set(scope, k2.into(), v2.into()).unwrap();
+           obj.set(scope, k3.into(), v3.into()).unwrap();
+        });
+    });
+}
